a90343d0dae0ec28efd8271f2e25141d1ea6c28d
Chris.Drm <jonh00ksksks@gmail.com>
2026-07-06 01:56:42 -0300

    Update Teste_BarramentoIR_copy_20260704200557.ino
    Melhorei algo que eu tinha notado, mas não sei se é definitivo, vou dedicar meu tempo no sensor de cor TCS3200 que meu prof me ajudou em tudo, para fazer o meu robo andar ate o começo do novo semestre...
    Tlvz seja o ultimo commit para esse codigo

--- Teste_BarramentoIR_copy_20260704200557.ino
+++ Teste_BarramentoIR_copy_20260704200557.ino
@@ -1,6 +1,5 @@
 const int pin[6] = {A8, A9, A10, A11, A12, A13};
 int leitura[6];
-int L[6];
 
 void setup() {
   Serial.begin(9600);
@@ -21,19 +20,27 @@ void loop()
 
   for(int i = 0; i < 6; i++)
     {
-      leitura[i] = analogRead(pin[i]);
-
+      leitura[i] = analogRead(pin[i]);  // armazena valores de 0 -1023 -- 8bits
+      /*
+      // trasnforma esses valores para bitnario para facilitar o segue linha
+      // No meu caso dos sensores entre escuro e claro (braco e preto) meu sensor lia de ~230 ate ~370
+      // então eu tranformei em binario para fazer uma forma simplifcada para fazer um segue linha
+      // Vou estudar como faz para generalizar isso, como calibrar os sensores com valores minimos e maximos
+      // para garar um binario ou algo assim para fazer o tratamento para fazer um segue linha ou um PID para
+      // o mesmo... 
+      // OBS: ainda estou estudando pouco a pouco, errando bastante e procurando ajuda
+      
       if(leitura[i] < 250)
       {
-        L[i] = 0;
+        leitura[i] = 0;
       }
       if(leitura[i] > 250)
       {
-        L[i] = 1;
+        leitura[i] = 1;
       }
-
+      */
       Serial.print(" ");
-      Serial.print(L[i]);
+      Serial.print(leitura[i]);
       Serial.print(" ");
     }
   Serial.println("");
